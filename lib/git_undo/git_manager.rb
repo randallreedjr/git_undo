b82d9d68b3a962ae7eb986b79bc9b2ec0cd49890
Randall Reed, Jr <randallreedjr@gmail.com>
2016-05-12 00:14:48 -0400

    Add setup if ENV variable isn't set

--- lib/git_undo/git_manager.rb
+++ lib/git_undo/git_manager.rb
@@ -23,4 +23,50 @@ class GitManager
     get_commands
     puts "Last git command was: `#{last_command}`"
   end
+
+  def self.setup
+    puts "It looks like you haven't run the initial setup. Would you like to do so now (y/n)?"
+    if gets.chomp.downcase == 'y'
+      puts "This will involve appending an alias to your .bash_profile. Okay to proceed?"
+      if gets.chomp.downcase == 'y'
+        puts "Thanks!"
+        update_bash_profile
+      else
+        puts "Goodbye!"
+      end
+    else
+      puts "Goodbye!"
+    end
+  end
+
+  def self.update_bash_profile
+    alias_exists = false
+    # last_line_alias = false
+    # position = 0
+    # line_count = 0
+    # index = 0
+    # max_line_count = 0
+    file = File.open(File.expand_path('~' + '/.bash_profile'),'r+')
+
+    file.readlines.each do |line|
+      # line_count += 1
+      if /\A[\s]*alias/.match line
+        if /\A[\s]*alias gitundo="HISTFILE=\$HISTFILE gitundo"\n\z/.match line
+          alias_exists = true
+        end
+        # max_line_count = line_count
+      # elsif last_line_alias
+      #   position = file.pos
+      #   last_line_alias = false
+      end
+    end
+    unless alias_exists
+      file.write("# Git Undo\n")
+      file.write("alias gitundo=\"HISTFILE=$HISTFILE gitundo\"\n")
+      puts "Please run `source ~/.bash_profile && cd .` to reload configuration"
+    end
+    file.close
+    # puts max_line_count + 1
+    # puts position
+  end
 end
