93262092c1f032ced5fb1e45ea5c7bb427cbe135
Randall Reed, Jr <randallreedjr@gmail.com>
2016-06-08 13:35:57 +0200

    Remove unnecessary comments

--- lib/git_undo/git_manager.rb
+++ lib/git_undo/git_manager.rb
@@ -92,25 +92,17 @@ class GitManager
 
   def self.update_bash_profile
     alias_exists = false
-    # last_line_alias = false
-    # position = 0
-    # line_count = 0
-    # index = 0
-    # max_line_count = 0
+
     file = File.open(File.expand_path('~' + '/.bash_profile'),'r+')
 
     file.readlines.each do |line|
-      # line_count += 1
       if /\A[\s]*alias/.match line
         if /\A[\s]*alias gitundo="HISTFILE=\$HISTFILE gitundo"\n\z/.match line
           alias_exists = true
         end
-        # max_line_count = line_count
-      # elsif last_line_alias
-      #   position = file.pos
-      #   last_line_alias = false
       end
     end
+
     unless alias_exists
       file.write("# Git Undo\n")
       file.write("alias gitundo=\"HISTFILE=$HISTFILE gitundo\"\n")
@@ -119,7 +111,5 @@ class GitManager
       puts "Please run `source ~/.bash_profile && cd .` to reload configuration"
     end
     file.close
-    # puts max_line_count + 1
-    # puts position
   end
 end
